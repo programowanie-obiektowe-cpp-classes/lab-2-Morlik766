e0167f4df9948592c63da8b2d0449965da115e69
Mikołaj Orlik <01171673@pw.edu.pl>
2024-11-03 21:26:27 +0100

    zmiany

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,35 @@
 #pragma once
-
 #include "Resource.hpp"
+#include <memory>
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+public:
+    ResourceManager() : resource(std::make_unique< Resource >()) {}
+    ResourceManager(const ResourceManager& other)
+        : resource(std::make_unique< Resource >(*other.resource))
+    {}
+
+    ResourceManager(ResourceManager&& other) noexcept : resource(std::move(other.resource)) {}
+
+    ResourceManager& operator=(const ResourceManager& other)
+    {
+        if (this != &other) {
+            resource = std::make_unique< Resource >(*other.resource);
+        }
+        return *this;
+    }
+
+    ResourceManager& operator=(ResourceManager&& other) noexcept
+    {
+        if (this != &other) {
+            resource = std::move(other.resource);
+        }
+        return *this;
+    }
+
+    double get() const { return resource->get(); }
+
+private:
+    std::unique_ptr< Resource > resource;
 };
